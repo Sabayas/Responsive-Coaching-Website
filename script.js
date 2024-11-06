75ca53c5cfaf34e260ce7039dc15d3b574889a8a
Sabayas <sabayasmin345@gmail.com>
2024-11-06 21:18:00 +0530

    Added media query and side-bar

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideIcon = document.getElementById("side-icon");
+const menuBar = document.querySelector('.navbar');
+
+sideIcon.addEventListener("click", function(){
+     menuBar.classList.toggle("navbar")
+   
+})
